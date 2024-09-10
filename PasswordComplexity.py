cf465418f1fe67c3c875871b739ead31112c6832
Policepigeon <policepigeon01@gmail.com>
2024-09-10 12:54:39 +0100

    i made a mistake

--- PasswordComplexity.py
+++ PasswordComplexity.py
@@ -2,6 +2,6 @@
 
 #this bit gets the password and checks that it is longer than fifteen characters long
 password = input("what is your desired password?")
-while int(len(password) < 15:
+while int(len(password)) < 15:
     input("Password too short, please input a new password")
 
