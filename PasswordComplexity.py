3fe5df0b5f325462669f0d0593ab858a01b995fd
Policepigeon <policepigeon01@gmail.com>
2024-09-10 12:53:52 +0100

    edit

--- PasswordComplexity.py
+++ PasswordComplexity.py
@@ -1,5 +1,7 @@
 #this program is about passowrd complexity. its for mr barrow
 
+#this bit gets the password and checks that it is longer than fifteen characters long
 password = input("what is your desired password?")
 while int(len(password) < 15:
     input("Password too short, please input a new password")
+
