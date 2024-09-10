0d523884c8d5146f22f8d92235d3ff02fdb2f96c
Policepigeon <policepigeon01@gmail.com>
2024-09-10 14:19:38 +0100

    i made it work, and now i think i will use the same system for everything else

--- PasswordComplexity.py
+++ PasswordComplexity.py
@@ -9,16 +9,33 @@ lengthcheck = False
 hasnumber = False
 
 #this bit gets the password and checks that it is longer than fifteen characters long
-password = input("what is your desired password?")
+
+password = "short"
+uppers = []
+lowers = []
+
 # i have to make a list for this password to get the upper/lower
+
+password = input("Please enter your desired password:")
+
+#this here checks the ascii code of the letter and adds it to a list if the number is between the ascii code ranges for a upper/lower case letter
+
 uppers = [l for l in password if ord(l) >= 65 and ord(l) <= 90] 
 lowers = [l for l in password if ord(l) >= 97 and ord(l) <= 122]
-while int(len(password)) < 15:
-    passoword = input("Password too short, please input a new password")
+
+
+while int(len(password)) < 15 or len(uppers) <= 1 or len(lowers) <= 1:
+    password = input("Password invalid, please remember that your password must have: \n at least one uppercase, \n at least one lowercase, \n at least one special character, \n at least one number, \n and be at least fifteen characters in length. \n Enter an amended password:")
     uppers = [l for l in password if l.isupper()]
     lowers = [l for l in password if l.islower()]
     print(uppers)
     print(lowers)
+
+#setting the variables to true
+hasupper = True
+haslower = True
+
+    
 
 
 
