03ec3781d56037a09cab3de1a3c9beb93077a4bd
Policepigeon <policepigeon01@gmail.com>
2024-09-10 12:52:43 +0100

    school stuff at school

---
+++ PasswordComplexity.py
@@ -0,0 +1,5 @@
+#this program is about passowrd complexity. its for mr barrow
+
+password = input("what is your desired password?")
+while int(len(password) < 15:
+    input("Password too short, please input a new password")
