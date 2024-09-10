43e3ddc787279f440da4ef39e09aab1e001c301f
Policepigeon <policepigeon01@gmail.com>
2024-09-10 14:46:29 +0100

    committing taketwo to git as it has a more efficient way of managing the password than takeone

--- PasswordComplexityTakeTwo.py
+++ PasswordComplexityTakeTwo.py
@@ -12,8 +12,48 @@ hasnumber = False
 
 #making the password
 password = input("please input your password")
-if set(password).difference(ascii_letters + diigts):
-    print("hasspecial")
+
+#these check the string library for whether it has an intersection of the desired character set and the character set in the password
+if set(password).difference(ascii_letters + digits):
+    hasspecial = True
 else:
-    print("hasntspecials")
-    
+    hasspecial = False
+
+if set(password).intersection(ascii_lowercase):
+    haslower = True
+else:
+    haslower = False
+
+if set(password).intersection(ascii_uppercase):
+    hasupper = True
+else:
+    hasupper = False
+
+if set(password),intersection(digits):
+    hasnumber = True
+else:
+    hasnumber = False
+
+while len(password) < 15 or hasupper = False or haslower = False or hasspecial = false or hasnumber = false:
+    password = input("please input your password")
+
+    #these check the string library for whether it has an intersection of the desired character set and the character set in the password
+    if set(password).difference(ascii_letters + digits):
+        hasspecial = True
+    else:
+        hasspecial = False
+
+    if set(password).intersection(ascii_lowercase):
+        haslower = True
+    else:
+        haslower = False
+
+    if set(password).intersection(ascii_uppercase):
+        hasupper = True
+    else:
+        hasupper = False
+
+    if set(password),intersection(digits):
+        hasnumber = True
+    else:
+        hasnumber = False
