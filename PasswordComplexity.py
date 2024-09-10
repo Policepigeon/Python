93ec61b4c5bec40393a966e70b7b93d36857fcdd
Policepigeon <policepigeon01@gmail.com>
2024-09-10 13:05:37 +0100

    lunch commit

--- PasswordComplexity.py
+++ PasswordComplexity.py
@@ -1,7 +1,24 @@
 #this program is about passowrd complexity. its for mr barrow
 
+
+#defining variables
+hasupper = False
+haslower = False
+hasspecial = False
+lengthcheck = False
+hasnumber = False
+
 #this bit gets the password and checks that it is longer than fifteen characters long
 password = input("what is your desired password?")
+# i have to make a list for this password to get the upper/lower
+uppers = [l for l in password if ord(l) >= 65 and ord(l) <= 90] 
+lowers = [l for l in password if ord(l) >= 97 and ord(l) <= 122]
 while int(len(password)) < 15:
     input("Password too short, please input a new password")
+    uppers = [l for l in password if l.isupper()]
+    lowers = [l for l in password if l.islower()]
+    print(uppers)
+    print(lowers)
+
+
 
