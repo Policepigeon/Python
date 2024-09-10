0d1f34899bb69aa001c7e7e8bd3179e410dd4f8c
Policepigeon <policepigeon01@gmail.com>
2024-09-10 19:35:59 +0100

    outside commit while i wait to go home

--- PasswordComplexityTakeTwo.py
+++ PasswordComplexityTakeTwo.py
@@ -29,12 +29,12 @@ if set(password).intersection(ascii_uppercase):
 else:
     hasupper = False
 
-if set(password),intersection(digits):
+if set(password).intersection(digits):
     hasnumber = True
 else:
     hasnumber = False
 
-while len(password) < 15 or hasupper = False or haslower = False or hasspecial = false or hasnumber = false:
+while len(password) < 15 or hasupper == False or haslower == False or hasspecial == False or hasnumber == False:
     password = input("please input your password")
 
     #these check the string library for whether it has an intersection of the desired character set and the character set in the password
@@ -52,8 +52,23 @@ while len(password) < 15 or hasupper = False or haslower = False or hasspecial =
         hasupper = True
     else:
         hasupper = False
+    #these check the string library for whether it has an intersection of the desired character set and the character set in the password
+    if set(password).difference(ascii_letters + digits):
+        hasspecial = True
+    else:
+        hasspecial = False
 
-    if set(password),intersection(digits):
+    if set(password).intersection(ascii_lowercase):
+        haslower = True
+    else:
+        haslower = Falsepassword = input("please input your password")
+
+    if set(password).intersection(digits):
         hasnumber = True
     else:
         hasnumber = False
+
+    if set(password).intersection(ascii_uppercase):
+        hasupper = True
+    else:
+        hasupper = Falsepassword = input("please input your password")
