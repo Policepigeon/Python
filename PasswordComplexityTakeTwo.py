d6705100d6a5c185b744baa2bf515895ceb60fa3
Policepigeon <policepigeon01@gmail.com>
2024-09-11 12:00:12 +0100

    it works

--- PasswordComplexityTakeTwo.py
+++ PasswordComplexityTakeTwo.py
@@ -4,15 +4,13 @@ from string import ascii_letters, digits, ascii_lowercase, ascii_uppercase, punc
 import getpass
 
 
-# testing what getpass does
-shrersh = getpass.getpass()
-print(shrersh)
 # Define a function to check password complexity
-def checks(password):
+def checks(password, password2):
     hasupper = False
     haslower = False
     hasspecial = False
     hasnumber = False
+    confirmed = False
 
     # Check if the password has any special characters
     if set(password).difference(ascii_letters + digits) & set(punctuation):
@@ -30,17 +28,23 @@ def checks(password):
     if set(password).intersection(digits):
         hasnumber = True
 
-    return hasupper, haslower, hasspecial, hasnumber
+    if password == password2:
+        confirmed = True
+    else:
+        print("passwords are not the same")
+    return hasupper, haslower, hasspecial, hasnumber, confirmed
 
 # Prompt user to input their password
-password = input("Please input your password: ")
-
+password = getpass.getpass("Please enter a password")
+password2 = getpass.getpass("Please confirm the password")
 # Continue prompting the user until the password meets the complexity requirements
 while len(password) < 15:
-    hasupper, haslower, hasspecial, hasnumber = checks(password)
+    hasupper, haslower, hasspecial, hasnumber, confirmed = checks(password, password2)
     if not (hasupper and haslower and hasspecial and hasnumber):
         print("Password is not complex enough. Please make sure it contains: \n at least one upper case character, \n at least one lower case character, \n at least one number, \n at least one special character, \n and is at least fifteen characters in length.")
-        password = input("Please input your password: ")
+        password = getpass.getpass("Please enter an amended password")
+        password2 = getpass.getpass("Please confirm the password")
+
     else:
         break
 
