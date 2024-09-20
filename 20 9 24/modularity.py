c66d9fd43cedc582e3700de3f20762be64a6ea39
Policepigeon <policepigeon01@gmail.com>
2024-09-20 12:46:36 +0100

    pir

---
+++ 20 9 24/modularity.py
@@ -0,0 +1,13 @@
+#this is  aprogram that should use procedures
+
+#function to get the value of pi r
+def pir(radius):
+    pir = 3.14159 * radius
+    return pir
+
+#now calculate radius
+radius = float(input("please input radius"))
+area = pir * radius
+circumference = pir * 2
+print("area:", area)
+print("curcumference", circumference)
