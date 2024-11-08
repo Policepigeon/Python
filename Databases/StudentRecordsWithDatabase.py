b6ffd73150a2592319263f09b0c3da9b16d997e4
Policepigeon <policepigeon01@gmail.com>
2024-11-08 08:31:24 +0000

    sqeal

--- Databases/StudentRecordsWithDatabase.py
+++ Databases/StudentRecordsWithDatabase.py
@@ -1,8 +1,14 @@
 #a program that stores and manages student records
+#importing the sql linker
+import sqlite3
+con = sqlite3.connect("Student DB")
+cur = con.cursor()
+#this is needed the first time you run the program to create the database
+#cur.execute("CREATE TABLE student(name, lastname, prefername, dbirth, house, lastgrade, detention)")
 
 class Student:
   #the basic student class
-    def __init__(self, name, house, lastgrade, prefername, dbirth, schoolclass, detention, lname ):
+    def __init__(self, name, house, lastgrade, prefername, dbirth, schoolclass, detention, lastname ):
         self.name = name
         self.house = house
         self.lastgrade = lastgrade
@@ -10,7 +16,7 @@ class Student:
         self.dbirth = dbirth
         self.schoolclass = schoolclass
         self.detention = detention
-        self.lname = lname
+        self.lastname = lastname
 
 #now i spend my days creating getters and setters
 
@@ -25,6 +31,16 @@ class Student:
       self.schoolclass = schoolclass
 
 andy = Student("andrew", "evans", "9", "andy", "17 9 24", "12F", "False", "crawford")
+#taking the input for the student
+student = input("what is the student's name?")
+student = Student(student, input("\n What is the student's house?", ))
 print(andy.prefername)
 
+#Allows the user to input their desired query, for example a search for users. do input sanitation later.
+res = cur.execute(input("Please input your SQLite query"))
+print(res.fetchall())
 
+#closing the cursor and connection to the db after commiting the changes to the database
+con.commit()
+cur.close()
+con.close()
